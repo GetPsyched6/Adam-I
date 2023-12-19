5f470c342a95d639e7563512b0a1cb97581f9958
Moahamed Jaasir <jaasir.subair@gmail.com>
2023-12-20 00:16:41 +0400

    Forgot password screen added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Register from './components/Register/Register';
 import Login from './components/Login/Login';
 import Home from './pages/Home/Home';
+import ForgotPassword from './components/Forgotpassword/ForgotPassword';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
       </Routes>
     </div>
   );
