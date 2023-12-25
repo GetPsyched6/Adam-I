872fe595652c5e1f370d055ede7e25ed85ced6df
Ankur7891 <ankurmajumdar7891@gmail.com>
2023-12-25 19:07:25 +0530

    Home Page: Sec-1 with 3-Type Device Responsive WebD

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import './Home.css';
+// import NavBar from '../../components/Navbar/Navbar';
+import Section1 from './Section1';
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="home-main">
+      {/* <NavBar /> */}
+      <Section1 />
+    </div>
+  );
 }
 
 export default Home;
---
+++ src/pages/Home/Section1.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Section1.css';
+
+function Section1() {
+  return (
+    <div className="home-sec1">
+      <div className="s1-img">
+        <div className="s1-text">
+          <p className="h1">Invest in Africa</p>
+          <p className="h2">
+            Shaping Tomorrow Together,
+            <br />
+            Invest in Africa&apos;s Potential.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Section1;
