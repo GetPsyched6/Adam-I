7005bf67e6d1af37aad3eda50cfd4ecbc6c222f8
GetPsyched6 <roshin006@gmail.com>
2024-02-25 13:34:13 +0530

    Revert "initial switch from rtg to framer-motion"
    This reverts commit e464fd8a4969672240bec205248d6a55685e5a70.

--- src/pages/Home/SectionOne/SectionOne.jsx
+++ src/pages/Home/SectionOne/SectionOne.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import styles from './SectionOne.module.css';
 import backgroundOne from '../../../assets/images/Home/home-section-1.webp';
 import backgroundTwo from '../../../assets/images/Home/home-section-2.webp';
@@ -21,10 +22,21 @@ function SectionOne() {
 
   return (
     <div className={styles.wrapper}>
-      <div
-        className={styles.background}
-        style={{ backgroundImage: `url(${backgrounds[activeImage]})` }}
-      />
+      <TransitionGroup>
+        <CSSTransition
+          key={backgrounds[activeImage]}
+          classNames={{
+            ...styles,
+          }}
+          timeout={500}
+          unmountOnExit
+        >
+          <div
+            className={styles.background}
+            style={{ backgroundImage: `url(${backgrounds[activeImage]})` }}
+          />
+        </CSSTransition>
+      </TransitionGroup>
       <div className={styles.content}>
         <h1 className={`${styles.text} ${styles.title}`}>Invest in Africa</h1>
         <h4 className={`${styles.text} ${styles.subtitle}`}>
