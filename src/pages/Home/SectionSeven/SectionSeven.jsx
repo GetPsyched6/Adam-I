8559d0c8ecdd0132fd5c5b7e04edd4b6208f8542
GetPsyched6 <roshin006@gmail.com>
2024-02-28 00:36:15 +0530

    added parallax to section 7

--- src/pages/Home/SectionSeven/SectionSeven.jsx
+++ src/pages/Home/SectionSeven/SectionSeven.jsx
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { motion, useScroll, useTransform, useReducedMotion } from 'framer-motion';
 import styles from './SectionSeven.module.css';
 import ContactUs from '../../../components/Button/OutlinedButton/OutlinedButton';
 
 function SectionSeven() {
+  const ref = useRef(null);
+  const shouldReduceMotion = useReducedMotion();
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ['start end', 'end end'],
+  });
+  const motionY = useTransform(scrollYProgress, [0, 1], [0, 300]);
+  const y = shouldReduceMotion ? 0 : motionY;
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.content}>
-        <ContactUs />
+    <motion.div className={styles.wrapper} ref={ref}>
+      <motion.div className={styles.content} style={{ y }}>
         <h2 className={styles.title}>
           Partner with us to build
           <br />
           Prosperity for Africa.
         </h2>
-      </div>
-    </div>
+        <ContactUs />
+      </motion.div>
+    </motion.div>
   );
 }
 
