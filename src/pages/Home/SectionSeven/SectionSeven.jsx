1cdcf6523d8ffed56d069c2835c43b18a8f9766e
GetPsyched6 <roshin006@gmail.com>
2024-02-29 18:42:31 +0530

    more responsive parallax

--- src/pages/Home/SectionSeven/SectionSeven.jsx
+++ src/pages/Home/SectionSeven/SectionSeven.jsx
@@ -1,17 +1,26 @@
 import React, { useRef } from 'react';
 import { motion, useScroll, useTransform, useReducedMotion } from 'framer-motion';
+import { useMediaQuery } from '@react-hook/media-query';
 import styles from './SectionSeven.module.css';
 import ContactUs from '../../../components/Button/OutlinedButton/OutlinedButton';
 
+function getMaxPercentage(isNotDesktop) {
+  if (isNotDesktop) return '35vh';
+  return '40vh';
+}
+
 function SectionSeven() {
+  const isNotDesktop = useMediaQuery('(max-width: 992px)');
+
   const ref = useRef(null);
   const shouldReduceMotion = useReducedMotion();
   const { scrollYProgress } = useScroll({
     target: ref,
     offset: ['start end', 'end end'],
   });
-  const motionY = useTransform(scrollYProgress, [0, 1], [0, 300]);
-  const y = shouldReduceMotion ? 0 : motionY;
+  const maxPercentage = getMaxPercentage(isNotDesktop);
+  const motionY = useTransform(scrollYProgress, [0, 1], ['0vh', maxPercentage]);
+  const y = shouldReduceMotion ? '0vh' : motionY;
   return (
     <motion.div className={styles.wrapper} ref={ref}>
       <motion.div className={styles.content} style={{ y }}>
