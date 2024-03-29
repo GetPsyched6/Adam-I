208c185cc24c87ec2416598ecc69caabd22e36bf
gitmarcher <153539985+gitmarcher@users.noreply.github.com>
2024-03-29 23:58:56 +0530

    Revert "Added components in Nigeria page"

--- src/pages/Nigeria/Nigeria.jsx
+++ src/pages/Nigeria/Nigeria.jsx
@@ -1,23 +1,10 @@
-import React from 'react';
-import InvestmentSupport from '../../components/Countries/InvestmentSupport/InvestmentSupport';
-import ImportExport from '../../components/Countries/ImportExport/ImportExport';
-import ImportSubstituition from '../../components/Countries/ImportSubstituition/ImportSubstituition';
 import SectionThree from './SectionThree/SectionThree';
 import SectionFour from './SectionFour/SectionFour';
-
 import styles from './Nigeria.module.css';
 
 function Nigeria() {
   return (
-
     <div className={styles.card_wrapper} id="nigeria" role="main">
-            <h1>Nigeria</h1>
-      <InvestmentSupport />
-      <br />
-      <ImportExport />
-      <br />
-      <ImportSubstituition />
-      <br />
       <SectionThree />
       <SectionFour />
     </div>
