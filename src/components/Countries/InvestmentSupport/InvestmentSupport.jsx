cc26075755efe5918bb94a24cc2557d3ade9ce38
Amogh-4 <jaasir.subair@gmail.com>
2024-03-29 11:00:47 +0400

    added components to nigeria page

---
+++ src/components/Countries/InvestmentSupport/InvestmentSupport.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import nipc from '../../../assets/nipc.png';
+import styles from './InvestmentSupport.module.css';
+
+function InvestmentSupport() {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.content}>
+        <h2 className={styles.title}>Investment Support and Resources</h2>
+
+        <div className={styles.info}>
+          <p>
+            Nigerian Investment Promotion Commission (NIPC): The NIPC serves as your guide to investing
+            in Nigeria. They offer valuable insights, facilitate business registration, and provide
+            investment incentives.
+          </p>
+          <img src={nipc} className={styles.logo}/>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default InvestmentSupport;
--- src/pages/Nigeria/Nigeria.jsx
+++ src/pages/Nigeria/Nigeria.jsx
@@ -1,7 +1,20 @@
+import React from "react";
+
+import InvestmentSupport from "../../components/Countries/InvestmentSupport/InvestmentSupport";
+import ImportExport from "../../components/Countries/ImportExport/ImportExport";
+import ImportSubstituition from "../../components/Countries/ImportSubstituition/ImportSubstituition";
+import styles from './Nigeria.module.css';
+
 function Nigeria() {
   return (
-    <div>
+    <div className={styles.content}>
       <h1>Nigeria</h1>
+      <InvestmentSupport />
+      <br />
+      <ImportExport />
+      <br />
+      <ImportSubstituition />
+      <br />
     </div>
   );
 }
